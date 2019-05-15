a682cd24a022d1f9a145fe8060aed1ce8b85c425
George Crabtree <george@canva.com>
2019-05-15 14:03:34 +1000

    Add cli

---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+const execa = require('execa');
+const fs = require('fs');
+
+let hadError = [];
+
+try {
+  fs.accessSync('/usr/local/opt/webp/bin/cwebp');
+} catch (e) {
+  hadError.push('webp');
+}
+
+try {
+  fs.accessSync('/usr/local/opt/mozjpeg/bin/cjpeg');
+} catch (e) {
+  hadError.push('mozjpeg');
+}
+
+try {
+  fs.accessSync('/usr/local/opt/pngquant/bin/pngquant');
+} catch (e) {
+  hadError.push('pngquant');
+}
+
+if (hadError.length) {
+  console.error(`Missing dependencies please run \`brew install ${hadError.join(' ')}\``);
+} else {
+  updateNotifier({pkg}).notify();
+  execa('./dist/mac/pact-image-compressor.app/Contents/MacOS/pact-image-compressor', process.argv, {shell: true})
+    .catch(e => console.error(e));
+}
